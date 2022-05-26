b6aeed3e85cb7ce7e74ed2efd30ee67a648d01ad
Sagar <worthless.dumboo@gmail.com>
2022-05-26 17:41:35 -0500

    W1D4: Q2: Clean FunctionalProgramming Way inspired by @atmoprakash

--- Assignments/W1D4-Hibernate-2/src/main/java/edu/mum/cs544/App.java
+++ Assignments/W1D4-Hibernate-2/src/main/java/edu/mum/cs544/App.java
@@ -3,43 +3,52 @@ package edu.mum.cs544;
 import javax.persistence.*;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class App {
 
     private static EntityManagerFactory emf;
 
+    static void printAllStudents(){
+        apply(entityManager -> {
+            TypedQuery<Students> query = entityManager.createQuery("from edu.mum.cs544.Students",Students.class);
+            List<Students> studentsList = query.getResultList();
+            print(studentsList);
+        });
+    }
+
+    static void insertOne(){
+        apply(entityManager -> {
+            entityManager.persist(Students.builder().id(444).email("ram@email.com").name("RAM").build());
+        });
+    }
 
     public static void main(String[] args) {
 
         emf = Persistence.createEntityManagerFactory("cs544");
 
-        EntityManager em = emf.createEntityManager();
+        printAllStudents();
+        insertOne();
+        printAllStudents();
 
-        em.getTransaction().begin();
-        TypedQuery<Students> query = em.createQuery("from edu.mum.cs544.Students",Students.class);
-        List<Students> studentsList = query.getResultList();
-        print(studentsList);
-        em.getTransaction().commit();
-        em.close();
-
-        em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(Students.builder().id(455).email("ram@email.com").name("RAM").build());
-        em.getTransaction().commit();
-        em.close();
-
-        em=emf.createEntityManager();
-        em.getTransaction().begin();
-        TypedQuery<Students> query1 = em.createQuery("from edu.mum.cs544.Students",Students.class);
-        List<Students> students = query1.getResultList();
-        print(students);
-        em.getTransaction().commit();
-        em.close();
 
     }
 
     private static void print(List<Students> studentsList) {
 
         studentsList.forEach(s -> System.out.println("NAME: "+s.getName()+", EMAIL: "+s.getEmail()));
+    }
+
+    private static void apply(Consumer<EntityManager> consumer){
+        try {
+            final EntityManager em  = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            consumer.accept(em);
+            em.getTransaction().commit();
+            em.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
